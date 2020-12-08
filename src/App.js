3d66fb8b4e79767be7cb90470be2568bdadd2673
Ben Shabtai <ben.shabtai@gmail.com>
2020-12-08 18:23:57 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,161 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import favorite_filled from './icons/favorite-24px-filled.svg'
+import favorite_outlined from './icons/favorite-24px-outlined.svg'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FilmList />
     </div>
   );
 }
+
+/**
+ * Fetches the API data and then passes it down
+ * to it's child for rendering (FaCC).
+ */
+class FetchFilms extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      films: []
+    };
+  }
+
+  // Seperate API calls to a different function
+  componentDidMount() {
+    fetch("https://swapi.dev/api/films/")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            films: result.results
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  // Function as Child Component (FaCC)
+  render() {
+    const { error, isLoaded, films } = this.state;
+    return this.props.children({ error, isLoaded, films });
+  }
+}
+
+FetchFilms.propTypes = {
+  children: PropTypes.func.isRequired,
+};
+
+/**
+ * Renders the result of FetchFilms' API call
+ */
+function FilmList() {
+  return (
+    <FetchFilms>
+      {({ error, isLoaded, films }) => {
+        // Handle error
+        if (error) return <div>Error: {error.message}</div>;
+        // Handle loading
+        else if (!isLoaded) return <div>Loading...</div>;
+        // Handle loaded
+        else {
+          return (
+            <ul className="film-list">
+              {films.map(film => (
+                <Film key={film.episodeId} episodeId={film.episode_id} title={film.title}
+                  director={film.director} producer={film.producer} releaseDate={film.release_date} />
+              ))}
+            </ul>
+          );
+        }
+      }}
+    </FetchFilms>
+  )
+}
+
+/**
+ * Get the Local Storage State of a film
+ */
+function getFilmLocalStorage(episodeId) {
+  // Get local storage state
+  let fav = localStorage.getItem(episodeId);
+
+  // Create local storage state if it doesn't exist
+  if (!fav) {
+    localStorage.setItem(episodeId, false);
+    fav = false;
+  }
+
+  // Convert to boolean
+  return fav == 'true';
+}
+
+/**
+ * Stateful component which renders a film list element
+ * and its 'Favorite' button according to local storage state
+ * and user input
+ */
+class Film extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Get the local storage state of this film
+    let fav = getFilmLocalStorage(props.episodeId);
+
+    this.state = {
+      isFavorite: fav,
+    };
+
+    this.handleFavorite = this.handleFavorite.bind(this);
+  }
+
+  handleFavorite() {
+    // Toggle isFavorite in local storage
+    localStorage.setItem(this.props.episodeId, !this.state.isFavorite)
+
+    // Toggle isFavorite in state
+    this.setState(state => ({
+      isFavorite: !state.isFavorite
+    }));
+  }
+
+  render() {
+    const { isFavorite } = this.state;
+
+    return (
+      <li key={this.props.episodeId} className="film">
+        <p><b>Title:</b> Episode {this.props.episodeId}: {this.props.title}</p>
+        <p><b>Director:</b> {this.props.director}</p>
+        <p><b>Producer:</b> {this.props.producer}</p>
+        <p><b>Release Date:</b> {this.props.releaseDate}</p>
+        <button className="fav-btn" onClick={this.handleFavorite}>
+          {isFavorite ? <img src={favorite_filled} /> : <img src={favorite_outlined} />}
+        </button>
+      </li>
+    )
+  }
+}
+
+Film.propTypes = {
+  episodeId: PropTypes.number,
+  title: PropTypes.string,
+  director: PropTypes.string,
+  producer: PropTypes.string,
+  releaseDate: PropTypes.string,
+  isFavorite: PropTypes.bool
+}
+
 
 export default App;
